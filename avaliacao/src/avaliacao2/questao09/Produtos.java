a8544d1447fe24a12ab224ccb250a7369977ecbb
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-22 16:27:49 -0300

    classe produtos

---
+++ avaliacao/src/avaliacao2/questao09/Produtos.java
@@ -0,0 +1,13 @@
+package avaliacao2.questao09;
+
+import java.sql.Date;
+
+public class Produtos {
+	
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private Double desconto;
+	private Date data;
+
+}
